c2c93605f34cf231132d32c7a998e6d1c901ee97
Evan <mr.evan.gordon@gmail.com>
2024-06-19 00:04:30 +0100

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import Nav from './components/Navbar';
 // const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Evan Gordon, Frontend developer',
-  description: 'My portfolio',
+  title: 'Evan Gordon',
+  description: ' Frontend developer',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
