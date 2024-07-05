77ea557ff8e1f6339880eb17f83a200683d8ffd1
Evan <mr.evan.gordon@gmail.com>
2024-07-05 22:58:33 +0100

    ui fix

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -7,58 +7,59 @@ import {
   Flex,
   Text,
   Kbd,
-  useColorModeValue
+  useColorModeValue,
 } from '@chakra-ui/react';
 import Image from './Image';
 
-import profiler from '../assets/LinkedIn.png'
+import profiler from '../assets/LinkedIn.png';
 import React from 'react';
-
 
 export default function About() {
   return (
     <Container mt={5} maxW={'3xl'}>
       <Box position={'relative'}>
-      <Box zIndex="0" width={{base: '100%', md: "60%"}} position="absolute" height={{base: '35%', md: "40%"}}>
-            <Box
-              bgGradient={useColorModeValue(
-                'radial(blue.600 1px, transparent 1px)',
-                'radial(blue.300 1px, transparent 1px)',
-              )}
-              backgroundSize="20px 20px"
-              opacity="0.4"
-              height="100%"
-            />
-          </Box>
+        <Box
+          zIndex="0"
+          width={{ base: '100%', md: '60%' }}
+          position="absolute"
+          height={{ base: '35%', md: '40%' }}
+        >
+          <Box
+            bgGradient={useColorModeValue(
+              'radial(blue.600 1px, transparent 1px)',
+              'radial(blue.300 1px, transparent 1px)',
+            )}
+            backgroundSize="20px 20px"
+            opacity="0.4"
+            height="100%"
+          />
+        </Box>
         <Stack>
           <Flex zIndex="1">
             <Image
-           
-         className='imgbg'
+              className="imgbg"
+              filter="grayscale(100%)"
               objectFit={'cover'}
               boxSize={24}
               rounded={'full'}
-              
-              alt='profiler'
-              src={
-                profiler
-              }
+              alt="profiler"
+              src={profiler}
             />
-            <Heading ml={5} mt={7} textAlign={'center'} fontSize={'xl'}>
+            <Heading ml={5} mt={9} textAlign={'center'} fontSize={'xl'}>
               I&apos;m Evan, welcome to my site 👋
             </Heading>
           </Flex>
-  
+
           <Text mt={4} fontSize={'xl'}>
-            I&apos;m a frontend developer. My expertise lies in <Kbd borderColor={'blue.400'}>React</Kbd>{' '}
-            and <Kbd borderColor={'white'}>Next.js</Kbd>.
+            I&apos;m a frontend developer. My expertise lies in{' '}
+            <Kbd borderColor={'blue.400'}>React</Kbd> and{' '}
+            <Kbd borderColor={'white'}>Next.js</Kbd>.
           </Text>
           <Text mt={4} fontSize={'xl'}>
-            Here to showcase my work, as well my learnings along the way in the hopes of
-            helping others.
+            Here to showcase my work, as well my learnings along the way in the
+            hopes of helping others.
           </Text>
         </Stack>
-   
       </Box>
     </Container>
   );
--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,52 +1,73 @@
-import { Box, Container, Divider, Flex, Heading,Text,
-  
+import {
+  Box,
+  Container,
+  Divider,
+  Flex,
+  Heading,
+  Text,
   ListItem,
-
   UnorderedList,
-} from '@chakra-ui/react'
-import React from 'react'
+} from '@chakra-ui/react';
+import React from 'react';
 
 export default function page() {
   return (
     <>
-        <Container maxW={'3xl'}>
-<Box
-mt={8}
-boxShadow='dark-lg'
-borderWidth="5px" 
-borderRadius={'md'}
-bgGradient={[
-  'linear(to-t, #4A5568, #A0AEC0)',
-]}
->
-<Box 
- bg='
- #1a202c'
- 
+      <Container maxW={'3xl'}>
+        <Box
+          mt={8}
+          boxShadow="dark-lg"
+          borderWidth="5px"
+          borderRadius={'md'}
+          bgGradient={['linear(to-t, #4A5568, #A0AEC0)']}
+        >
+          <Box
+            bg="
+ #1a202c"
+          >
+            <Heading color={'white'} m={2}>
+              {' '}
+              Experience
+            </Heading>
+            <Divider />
+            <Flex justifyContent={'space-between'}>
+              <Heading m={2} fontSize={'2xl'} color={'white'}>
+                {' '}
+                Rakeoff labs
+              </Heading>
+              <Text m={2} color={'white'}>
+                May &apos;23 - April &apos;24
+              </Text>
+            </Flex>
+            <Text ml={2} color={'white'}>
+              Co-founder & Frontend developer
+            </Text>
 
- >
-  
-  <Heading color={'white'}
-  m={2}> Experience</Heading>
-<Divider/>
-<Flex justifyContent={'space-between'}>
-<Heading m={2}fontSize={'2xl'} color={'white'}> Rakeoff labs</Heading>
-<Text m={2} color={'white'}>May &apos;23 - April &apos;24</Text>
-</Flex>
-<Text ml={2} color={'white'}>Co-founder & Frontend developer</Text>
-
-<Text m={2} mt={4} color={'white'}>Rakeoff, was a web3 app built on the ICP blockchain in which recieved a $25k developers grant for. This was my first introduction into software development in production. I learned a lot about coding best practices using React, UI/UX design principles, and how to market users towards our product. It was a great entrepreneurial experience building this startup for the year alongside my fellow classmate.
-</Text>
-<UnorderedList p={1} >
-  <ListItem color={'white'}>Built the landing page using React, Framer Motion and Styled with Chakra UI.</ListItem>
-  <ListItem color={'white'}>Built the analytics page using React, Recharts, Context API for state managment, and styled with Chakra.</ListItem>
-  <ListItem color={'white'}>Authored comprehensive documentation on GitBook.</ListItem>
-
-</UnorderedList>
- </Box>
- </Box>
-        </Container>
-
+            <Text m={2} mt={4} color={'white'}>
+              Rakeoff, was a web3 app built on the ICP blockchain in which
+              recieved a $25k developers grant for. This was my first
+              introduction into software development in production. I learned a
+              lot about best practices using React, UI/UX design principles, and
+              how to market users towards our product. It was a great
+              entrepreneurial experience building this startup for the year
+              alongside my fellow classmate.
+            </Text>
+            <UnorderedList p={1}>
+              <ListItem color={'white'}>
+                Built the landing page using React, Framer Motion and Styled
+                with Chakra UI.
+              </ListItem>
+              <ListItem color={'white'}>
+                Built the analytics page using React, Recharts, Context API for
+                state managment, and styled with Chakra.
+              </ListItem>
+              <ListItem color={'white'}>
+                Authored comprehensive documentation on GitBook.
+              </ListItem>
+            </UnorderedList>
+          </Box>
+        </Box>
+      </Container>
     </>
-  ) 
+  );
 }
