bb062cf8a1a2f012eb4feec9e65c5e640421b624
Evan <mr.evan.gordon@gmail.com>
2024-08-06 16:36:21 +0100

    nav

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,32 +1,45 @@
-import React from "react";
-import { getBlog, getBlogs } from "../../lib/data";
-import { Post } from "../../components/Post";
-import { notFound } from "next/navigation";
-import { Box, Container, Heading, Text, Flex } from "@chakra-ui/react";
+'use server';
+import React from 'react';
+import { getBlog, getBlogs } from '../../lib/data';
+import { Post } from '../../components/Post';
+import { notFound } from 'next/navigation';
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  Flex,
+  Breadcrumb,
+  BreadcrumbLink,
+} from '@chakra-ui/react';
 
 export async function generateStaticParams() {
   const blogs = await getBlogs();
-  return  blogs.map(( blog) => ({ slug: blog.slug }));
+  return blogs.map((blog) => ({ slug: blog.slug }));
 }
 
-export default async function Page({ params }: {
-  params: { slug: string } }) {
-
+export default async function Page({ params }: { params: { slug: string } }) {
   const blog = await getBlog(params.slug);
   if (!blog) return notFound();
 
   return (
     <>
-    <Container mt={4} maxW={'3xl'}>
-      <Flex justifyContent={'space-between'}>
-      <Heading fontSize={'5xl'}mt={4}>{blog.title}</Heading>
-      <Text fontSize={'xl'}color={'grey'}m={8}>{blog.date}</Text>
-      </Flex>
-      <Box mt={4}>
-      <Post >{blog.body}</Post>
-      </Box>
+      <Container mt={4} maxW={'3xl'}>
+        <Breadcrumb>
+          <BreadcrumbLink href="/"> Home</BreadcrumbLink>
+        </Breadcrumb>
+        <Flex justifyContent={'space-between'}>
+          <Heading fontSize={'5xl'} mt={4}>
+            {blog.title}
+          </Heading>
+          <Text fontSize={'xl'} color={'grey'} m={8}>
+            {blog.date}
+          </Text>
+        </Flex>
+        <Box mt={4}>
+          <Post>{blog.body}</Post>
+        </Box>
       </Container>
     </>
-  
   );
 }
